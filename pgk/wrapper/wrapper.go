4658593da69d2ca87576c35b657023e6baf6d9a3
dzania <artur.kantorczyk@gmail.com>
2021-08-26 16:53:25 +0200

    created login func

--- pgk/wrapper/wrapper.go
+++ pgk/wrapper/wrapper.go
@@ -1,13 +1,31 @@
 package wrapper
 
-type User struct {
-	username, password string
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Login user struct
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-func (u User) login() {
+// Takes username and string as arguments returns token on error response
+func Login(username string, password string) {
+	user := &LoginRequest{
+		Username: username,
+		Password: password,
+	}
+	userJSON, err = json.Marshal(user)
 
-}
+	resp, err := http.Post("http://localhost:8888/users/login", "application/json",
+		bytes.NewBuffer(userJSON))
 
-func (u User) register() {
-
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
 }
